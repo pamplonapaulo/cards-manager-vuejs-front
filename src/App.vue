e46e035fa6c808bdc113dbb905e502f427617d7c
Paulo Pamplona <pamplonapaulo@gmail.com>
2021-01-14 20:28:51 +0000

    card list - static

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <CreditCard name="Banco Original" img="card-green"/>
+  <CreditCard name="Banco BMG" img="card-gray"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CreditCard from './components/CreditCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreditCard
   }
 }
 </script>
---
+++ src/components/CreditCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <!-- <img alt="alt" src="../assets/card-green.png" /> -->
+    <img v-bind:src="'./src/assets/' + img + '.png'" /> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreditCard',
+  props: {
+    name: String,
+    img: String
+  }
+}
+</script>
